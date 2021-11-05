94f9d50e11f21b0ed88f94aa171d8e8da63e4535
YuZhaoYang <yuzhaoyang@osdiot.com>
2021-11-05 09:04:34 +0800

    将ideademo项目添加到本地库

---
+++ src/main/java/com/example/demo/test/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+    @RequestMapping(value = "/hello")
+    public String helloWorld(){
+        return "hello world";
+    }
+}
